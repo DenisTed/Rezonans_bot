341b03b26872ef2616a8340549a307536db9e15f
DenisTed <37961363+DenisTed@users.noreply.github.com>
2026-03-30 21:44:50 +0300

    v02

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 TOKEN = "8575672871:AAFHwULawzeGevvQ3nXzWddzLaPc2E5x-wI"
-WEB_APP_URL = "https://твій-сайт.netlify.app"
-ADMINS = [123456789]  # сюди потім вставиш свій Telegram ID
+WEB_APP_URL = "https://denisted.github.io/Rezonans_bot"
+ADMINS = [ 1363750803]  # сюди потім вставиш свій Telegram ID
