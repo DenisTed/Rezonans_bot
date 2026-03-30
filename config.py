6679f7ba74d97b264385486c26d9c032cad2614e
DenisTed <37961363+DenisTed@users.noreply.github.com>
2026-03-30 21:45:42 +0300

    v03

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 TOKEN = "8575672871:AAFHwULawzeGevvQ3nXzWddzLaPc2E5x-wI"
-WEB_APP_URL = "https://denisted.github.io/Rezonans_bot"
+WEB_APP_URL = "https://denisted.github.io/Rezonans_bot/"
 ADMINS = [ 1363750803]  # сюди потім вставиш свій Telegram ID
